66b70101baaf08d1aafe00b251ce8e5d0bb550d3
shemaamohamed <shimaaeldakhakhny@gmail.com>
2024-10-04 02:32:01 +0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,35 +32,62 @@ function NavBar() {
             <Nav.Link as={Link} to="/menu">
               Menu
             </Nav.Link>
-            <Nav.Link as={Link} to="/cart">
-              Cart
+            
+            <Nav.Link as={Link} to="/connect">
+              Contact
+            </Nav.Link>
+          </Nav>
+          <Nav>
+          <Nav.Link as={Link} to="/cart">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="1em"
-                height="1em"
+                width="2em"
+                height="1.5em"
                 stroke-linejoin="round"
                 stroke-linecap="round"
                 viewBox="0 0 24 24"
                 stroke-width="2"
                 fill="none"
                 stroke="currentColor"
+                color="black"
               >
                 <circle r="1" cy="21" cx="9"></circle>
                 <circle r="1" cy="21" cx="20"></circle>
                 <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
               </svg>
               <Badge
-                style={{ position: "relative", bottom: "15px", left: "7px" }}
+                style={{ position: "relative", bottom: "5px", left: "3px" }}
                 pill
                 bg="danger"
               >
                 {cart.length}
               </Badge>
             </Nav.Link>
-            <Nav.Link as={Link} to="/connect">
-              Contact
+            <Nav.Link as={Link} to="/">
+            <svg
+                class="icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20.503"
+                height="20.625"
+                viewBox="0 0 17.503 15.625"
+                color="black"
+              >
+                <path
+                  id="Fill"
+                  d="M8.752,15.625h0L1.383,8.162a4.824,4.824,0,0,1,0-6.762,4.679,4.679,0,0,1,6.674,0l.694.7.694-.7a4.678,4.678,0,0,1,6.675,0,4.825,4.825,0,0,1,0,6.762L8.752,15.624ZM4.72,1.25A3.442,3.442,0,0,0,2.277,2.275a3.562,3.562,0,0,0,0,5l6.475,6.556,6.475-6.556a3.563,3.563,0,0,0,0-5A3.443,3.443,0,0,0,12.786,1.25h-.01a3.415,3.415,0,0,0-2.443,1.038L8.752,3.9,7.164,2.275A3.442,3.442,0,0,0,4.72,1.25Z"
+                  transform="translate(0 0)"
+                ></path>
+              </svg>
+              <Badge
+                style={{ position: "relative", bottom: "5px", left: "5px" }}
+                pill
+                bg="danger"
+              >
+                {cart.length}
+              </Badge>
             </Nav.Link>
           </Nav>
+          
           <Nav>
             <NavDropdown
               style={{ marginLeft: "70px" }}
