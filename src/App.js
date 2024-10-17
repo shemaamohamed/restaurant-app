ddebe9a2b43042c80a30b7c78f61f9caa9bf9720
shemaamohamed <shimaaeldakhakhny@gmail.com>
2024-10-18 01:27:41 +0300

    Handle routes

--- src/App.js
+++ src/App.js
@@ -9,27 +9,53 @@ import OrderConfirmedPage from "./pages/OrderConfirmedPage.jsx";
 import WishListPage from "./pages/WishListPage.jsx";
 import LoginFormPage from "./pages/LoginFormPage.jsx";
 import SignUpFormPage from "./pages/SignUpFormPage.jsx";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 
-// import AdditemPage from "./Admin/AdditemPage.jsx";
-// import ProductListPage from "./Admin/ProductListPage.jsx";
-// import AdminProfile from "./Admin/AdminProfile.jsx";
-// import UpdateitemPage from "./Admin/UpdateitemPage.jsx";
-// import OrderPage from "./Admin/OrderPage.jsx";
-// import AdminLayout from "./Admin/AdminLayout.jsx";
-// import MessagesPage from "./Admin/MessagesPage.jsx";
-import { ThemeProvider, useTheme } from "@mui/material";
+import AdditemPage from "./Admin/AdditemPage.jsx";
+import ProductListPage from "./Admin/ProductListPage.jsx";
+import AdminProfile from "./Admin/AdminProfile.jsx";
+import UpdateitemPage from "./Admin/UpdateitemPage.jsx";
+import OrderPage from "./Admin/OrderPage.jsx";
+import AdminLayout from "./Admin/AdminLayout.jsx";
+import MessagesPage from "./Admin/MessagesPage.jsx";
+import {  ThemeProvider, useTheme } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function App() {
   const theme = useTheme();
+
+  const [usertype,setUsertype]=useState('user');
+  const [settoken,setToken]=useState('');
+  useEffect(()=>{
+    const intervalValue = setInterval(() => { 
+      const user =localStorage.getItem('type');
+      const token =localStorage.getItem('token');
+      if(user){
+        setUsertype(user);
+        setToken(token);
+       
+      
+      } else {
+        setUsertype('user');
+      }     
+    }, 1000)
+    return () => {
+      clearInterval(intervalValue)
+    }
+
+  },[]);
+
+  
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
+      {
+        usertype ==='user' ?( <>
+         <NavBar />
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="" element={<Homepage />} />
           <Route path="/connect" element={<Connectpage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/login" element={<LoginFormPage />} />
@@ -39,27 +65,29 @@ function App() {
           <Route path="order-confirmed" element={<OrderConfirmedPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
+
+        </>) : usertype ==='admin' ? (
+        <>
+        <ThemeProvider theme={theme}>
+        <Routes>
+           <Route path="" element={<AdminLayout />}>
+                <Route path="additems" element={<AdditemPage />} />
+                <Route path="productlist" element={<ProductListPage />} />
+                <Route path="updateitem" element={<UpdateitemPage />} />
+                <Route path="orders" element={<OrderPage />} />
+                <Route path="adminprofile" element={<AdminProfile />} />
+                <Route path="messages" element={<MessagesPage />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Route>
+          </Routes>
+          </ThemeProvider>
+          </>
+
+      ) :null
+      }
         <Toaster />
       </BrowserRouter>
     </div>
-    //====================================================================
-    // <ThemeProvider theme={theme}>
-    //   <div className="App">
-    //     <BrowserRouter>
-    //       <Routes>
-    //         <Route path="/" element={<AdminLayout />}>
-    //           <Route path="additems" element={<AdditemPage />} />
-    //           <Route path="productlist" element={<ProductListPage />} />
-    //           <Route path="updateitem" element={<UpdateitemPage />} />
-    //           <Route path="orders" element={<OrderPage />} />
-    //           <Route path="adminprofile" element={<AdminProfile />} />
-    //           <Route path="messages" element={<MessagesPage />} />
-    //         </Route>
-    //       </Routes>
-    //       <Toaster />
-    //     </BrowserRouter>
-    //   </div>
-    // </ThemeProvider>
   );
 }
 
