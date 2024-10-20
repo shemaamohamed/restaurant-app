e58d086c5a85ccd7a6dd63f15641602814ded89f
SolimanIslam <islam.salama.ie@gmail.com>
2024-10-20 23:40:26 +0300

    deployment url updates

--- backend/utils/frontendUrl.js
+++ backend/utils/frontendUrl.js
@@ -1,5 +1,5 @@
 // const frontend_url = 'http://localhost:3000';
-const frontend_url = '13.61.64.46:3000';
+const frontend_url = 'http://13.61.64.46:3000';
 
 
 export default frontend_url;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
 // const backendBaseUrl = "http://localhost:4000";
-const backendBaseUrl = "13.61.64.46:4000";
+const backendBaseUrl = "http://13.61.64.46:4000";
 
 export default backendBaseUrl;
