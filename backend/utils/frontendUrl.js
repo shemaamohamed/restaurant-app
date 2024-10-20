aac7bf74af8a700353691275127144cd45d7622e
SolimanIslam <islam.salama.ie@gmail.com>
2024-10-20 21:02:43 +0300

    build folder for aws deployment

--- backend/utils/frontendUrl.js
+++ backend/utils/frontendUrl.js
@@ -1,2 +1,5 @@
-const frontend_url = 'http://localhost:3000';
+// const frontend_url = 'http://localhost:3000';
+const frontend_url = '16.16.137.247:3000';
+
+
 export default frontend_url;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,4 @@
-const backendBaseUrl = "http://localhost:4000";
+// const backendBaseUrl = "http://localhost:4000";
+const backendBaseUrl = "16.16.137.247:4000";
+
 export default backendBaseUrl;
